5e4d4f118dd5b953d406bcb28d73163ea8c53dd9
binhex <megalith01@gmail.com>
2016-11-03 20:49:22 +0000

    use consolidated scripts

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,11 +3,52 @@
 # exit script if return code != 0
 set -e
 
+# build scripts
+####
+
+# download build scripts from github
+curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
+
+# pacman packages
+####
+
 # define pacman packages
 pacman_packages="php npm nodejs composer git mariadb libnotify php-fpm nginx"
 
-# install pre-reqs
-pacman -S --needed $pacman_packages --noconfirm
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages=""
+
+# define arch official repo (aor) package type e.g. core/community/extra
+aor_package_type=""
+
+# call aor script (arch official repo)
+source /root/aor.sh
+
+# aur packages
+####
+
+# define aur helper
+aur_helper="apacman"
+
+# define aur packages
+aur_packages=""
+
+# call aur install script (arch user repo)
+source /root/aur.sh
 
 # find latest koel release tag from github
 release_tag=$(curl -s https://github.com/phanan/koel/releases | grep -P -o -m 1 '(?<=/phanan/koel/releases/tag/)[^"]+')
@@ -46,6 +87,27 @@ echo "listen.owner = nobody" >> /etc/php/php-fpm.conf
 echo "" >> /etc/php/php-fpm.conf
 echo "; Specify user listener group" >> /etc/php/php-fpm.conf
 echo "listen.group = users" >> /etc/php/php-fpm.conf
+
+# container perms
+####
+
+# create file with contets of here doc
+cat <<'EOF' > /tmp/permissions_heredoc
+# set permissions inside container
+chown -R "${PUID}":"${PGID}" /opt/koel/ /usr/share/nginx/html/ /etc/nginx/ /etc/php/ /run/php-fpm/ /var/lib/nginx/ /var/log/nginx/ /var/lib/mysql/ /home/nobody
+chmod -R 775 /opt/koel/ /usr/share/nginx/html/ /etc/nginx/ /etc/php/ /run/php-fpm/ /var/lib/nginx/ /var/log/nginx/ /var/lib/mysql/ /home/nobody
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /root/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
 
 # cleanup
 yes|pacman -Scc
