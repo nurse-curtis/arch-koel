a0c6d141ff5aac48e8169714196473e17731b86a
binhex <megalith01@gmail.com>
2017-04-05 13:16:19 +0100

    fix laravel charset issue causing login issues

--- setup/root/custom.sh
+++ setup/root/custom.sh
@@ -14,3 +14,7 @@ yarn install --unsafe-perm
 # below is a hack to get around install issue for v3.4.1
 composer require pusher/pusher-php-server --no-scripts
 composer install
+
+# another hack, laravel 5.4 uses the utf8mb4 character set by default, which includes support for storing "emojis" in the database.
+# mysql 5.7.7+ / mariadb 10.2.2+) both have this new character set by default, so this is only a tempory hack until they are updated.
+sed -i -e 's~mb4~~g' /opt/koel/config/database.php
